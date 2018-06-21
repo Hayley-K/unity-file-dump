97066cc4d143295fdd3f09ad9f70823e2627ef93
Hayley-K <101098200@student.swin.edu.au>
2018-06-21 15:47:19 +1000

    adding unity files and a gitIgnore

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 Files I need to use between 2 computers
 
 basically, i'm testing some concepts in a unity project and will need access from multiple computers. 
+
+
+source tutorial: https://www.youtube.com/watch?v=ddakS7BgHRI
+source code: https://gist.github.com/ditzel/68be36987d8e7c83d48f497294c66e08
---
+++ practice prototype/Assets/script/cubeController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubeController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        //GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        //do i need cross paltform input instead? probably for controller
+
+        Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+	}
+}
