b9d6ecc7dc667709385f5802468246fbb8c7c4d6
Hayley-K <101098200@student.swin.edu.au>
2018-06-25 12:53:28 +1000

    idk

---
+++ practiceprototype/Assets/script/boxJump.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boxJump : MonoBehaviour {
+
+    public GameObject environment;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            GetComponent<parabolaController>().FollowParabola();
+            transform.SetParent(environment.transform);
+            
+            if (GetComponent<Rigidbody>() == null)
+                gameObject.AddComponent<Rigidbody>();
+        }
+	}
+}
---
+++ practiceprototype/Assets/script/pickup.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickup : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject pbRoot;
+    
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.P))
+        {
+            Destroy(GetComponent<Rigidbody>());
+            AddParabola();
+
+            Vector3 itemPosition = player.transform.position;
+            itemPosition.y += player.transform.GetComponent<Collider>().bounds.size.y;
+            itemPosition.y += GetComponent<Collider>().bounds.size.y / 2;
+
+            transform.Translate(itemPosition - transform.position, Space.World);
+            transform.SetParent(player.transform);
+            Quaternion currentFacing = transform.rotation;
+
+            //GameObject pbRootClone = Instantiate(pbRoot, transform.position, transform.rotation, transform);
+            
+        }
+	}
+    public void AddParabola()
+    {
+        gameObject.AddComponent<parabolaController>();
+        gameObject.GetComponent<parabolaController>().ParabolaRoot = Instantiate(pbRoot, transform.position, transform.rotation, transform);
+    }
+}
