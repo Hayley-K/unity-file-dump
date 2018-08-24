0088245f1151a8495a6ff5a7ae08b7056c142eed
unknown <101098200@AS409-010.ds.swin.edu.au>
2018-08-24 12:28:04 +1000

    searchAction

--- testingScriptables/Assets/scripts/Actions/SearchAction.cs
+++ testingScriptables/Assets/scripts/Actions/SearchAction.cs
@@ -13,5 +13,13 @@ public class SearchAction : Action {
     public void Search(StateController controller)
     {
         //ROTATE THE PLAYER 
+        float speed = 1f;
+        Quaternion platerStartRotation = controller.transform.rotation;
+        
+        //moving to a position not amount 
+        controller.transform.rotation = Quaternion.Slerp(platerStartRotation, Quaternion.Euler(0, -360, 0), Time.deltaTime * speed);
+        
+        //controller.transform.rotation = Quaternion.RotateTowards(platerStartRotation, Quaternion.Euler(0, 180, 0), Time.deltaTime * speed);
+        //controller.transform.rotation = Quaternion.Slerp(controller.transform.rotation, Quaternion.Euler(0, 90, 0), Time.deltaTime * speed);
     }
 }
